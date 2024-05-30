52dbfbb7b016dd802e0d4e6a7ef7aeff0e8b1355
Potti1234 <lukas.pottner@gmail.com>
2024-05-31 01:25:27 +0200

    Change Zoom of map to resonable values

--- wherehaveibeento/src/components/map.tsx
+++ wherehaveibeento/src/components/map.tsx
@@ -6,13 +6,20 @@ import 'leaflet/dist/leaflet.css';
 
 export default function Map() {
   return (
-    <div>
+    <div >
         <MapContainer
-            center={[51.505, -0.09]}
-            zoom={13}
-            scrollWheelZoom={true}
+            center={[30, 0]}
+            zoomControl={false}
+            maxZoom={15}
+            minZoom={3}
+            zoom={3}
+            zoomSnap={0.5}
+            zoomDelta={0.5}
+            wheelPxPerZoomLevel={120}
+            maxBoundsViscosity={0.5}
+            maxBounds={L.latLngBounds(new L.LatLng(-90, -180), new L.LatLng(90, 180))}
             style={{ height: "100vh", width: "100%" }}
-        >
+            >
             <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
