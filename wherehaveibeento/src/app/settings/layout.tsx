46d543895c0fcf99532f77159b50cd7d07dec8f2
Potti1234 <lukas.pottner@gmail.com>
2024-06-10 20:29:20 +0200

    Add settings + function to upload profile picture

---
+++ wherehaveibeento/src/app/settings/layout.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import SettingsNavigation from "@/components/settingsNavigation";
+import { createClient } from "@/utils/supabase/client";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const supabase = createClient();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  useEffect(() => {
+    supabase.auth.getUser().then(({ data, error }) => {
+      if (!data.user || error) {
+        return router.push("/login");
+      }
+    });
+  }, []);
+
+  return (
+    <SettingsNavigation
+      highlightedLink={pathname.split("/")[2]}
+      children={children}
+    ></SettingsNavigation>
+  );
+}
--- wherehaveibeento/src/components/headerDropdown.tsx
+++ wherehaveibeento/src/components/headerDropdown.tsx
@@ -12,21 +12,53 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { CircleUser} from "lucide-react"
+import Link from "next/link";
+import { createClient } from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
 
 export default function HeaderDropdown() {
+  const supabase = createClient();
+  const [profilePicture, setProfilePicture] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    supabase.auth.getUser().then(({ data, error }) => {
+      if (!data.user || error) {
+          return;
+      }
+
+      supabase.storage.from('profile_pictures').createSignedUrl(`${data.user.id}.png`, 3600).then(({ data, error }) => {
+        if (error) {
+          // No picture is uploaded
+          setProfilePicture(undefined);
+          return;
+        }
+
+        setProfilePicture(data.signedUrl);
+      });
+
+  });
+    }, [])
+   
   
   return (
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="secondary" size="icon" className="rounded-full">
-            <CircleUser className="h-5 w-5" />
+            {profilePicture ? (<img src={profilePicture} className="h-5 w-5 rounded-full" />) : (<CircleUser className="h-5 w-5" />)}
             <span className="sr-only">Toggle user menu</span>
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
           <DropdownMenuLabel>My Account</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuItem>Settings</DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link
+                href="/settings/general"
+                className="hover:text-foreground"
+              >
+                Settings
+              </Link>
+            </DropdownMenuItem>
           <DropdownMenuItem>Support</DropdownMenuItem>
           <DropdownMenuSeparator />
           <DropdownMenuItem onClick={() => {signOut()}}>Sign out</DropdownMenuItem>
