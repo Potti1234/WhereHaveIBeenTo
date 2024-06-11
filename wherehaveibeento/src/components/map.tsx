a0e54d62af75d0cafaa9c253ce253d3418537a0c
Potti1234 <lukas.pottner@gmail.com>
2024-06-11 23:26:50 +0200

    Saved visited cities get shown on the map with a marker

--- wherehaveibeento/src/components/map.tsx
+++ wherehaveibeento/src/components/map.tsx
@@ -1,10 +1,44 @@
 "use client"
 
-import { MapContainer, TileLayer} from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import React from 'react';
+import { createClient } from '@/utils/supabase/client';
+
+export const revalidate = 0
 
 export default function Map() {
+  const supabase = createClient()
+  const [cities, setCities] = React.useState<(any[])>([]);
+
+  const icon = L.icon({ iconUrl: "/images/marker-icon.png", iconSize: [25,41], iconAnchor: [12,41] });
+
+  const fetchVisitedCities = async (user_id : string) => {
+    const { data, error } = await supabase.from('visited_city')
+    .select(`city ( name, id, latitude, longitude)`)
+    .eq('user_id', user_id)
+
+    console.log(data)
+    if (error) {
+      console.error(error)
+    } else {
+      setCities(data)
+    }
+  }
+
+  React.useEffect(() => {
+    supabase.auth.getUser().then(({ data, error }) => {
+      if(error){
+        console.error("Error getting user", error)
+        return;
+      }
+      fetchVisitedCities(data.user.id)
+    })
+
+  }, [])
+
+
   return (
     <div >
         <MapContainer
@@ -20,6 +54,15 @@ export default function Map() {
             maxBounds={L.latLngBounds(new L.LatLng(-90, -180), new L.LatLng(90, 180))}
             style={{ height: "100vh", width: "100%" }}
             >
+              {cities.length > 0 &&
+                cities.map((city, index) => {
+                  return (
+                    <Marker position={[city.city.latitude, city.city.longitude]} key={index} icon={icon}>
+                      <Popup>{city.city.name}</Popup>
+                    </Marker>
+                  );
+     })}
+            
             <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
