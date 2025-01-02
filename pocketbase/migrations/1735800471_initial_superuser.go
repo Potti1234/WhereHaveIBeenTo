d693286afec8175423155acb037c6729bf45feb3
Potti1234 <lukas.pottner@gmail.com>
2025-01-02 13:09:44 +0100

    - Created a new `.env.dev` file for environment variables related to admin credentials. - Updated `.gitignore` to exclude `.env` and `pocketbasego.exe` files. - Added `github.com/joho/godotenv` as a dependency in `go.mod` to manage environment variables. - Implemented a migration script to create a superuser from the environment variables during the initial setup. - Refactored `main.go` to include migration command registration for automatic migration file creation in development.

---
+++ pocketbase/migrations/1735800471_initial_superuser.go
@@ -0,0 +1,40 @@
+package migrations
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		superusers, err := app.FindCollectionByNameOrId(core.CollectionNameSuperusers)
+		if err != nil {
+			return err
+		}
+
+		// load .env file
+		err = godotenv.Load(".env")
+
+		if err != nil {
+			log.Fatalf("Error loading .env file")
+		}
+
+		record := core.NewRecord(superusers)
+
+		record.Set("email", os.Getenv("ADMIN_EMAIL"))
+		record.Set("password", os.Getenv("ADMIN_PASSWORD"))
+
+		return app.Save(record)
+	}, func(app core.App) error { // optional revert operation
+		record, _ := app.FindAuthRecordByEmail(core.CollectionNameSuperusers, os.Getenv("ADMIN_EMAIL"))
+		if record == nil {
+			return nil // probably already deleted
+		}
+
+		return app.Delete(record)
+	})
+}
