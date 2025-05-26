4aedc67c823ff98603d9dab16e0004fa2a4a60b5
Potti1234 <lukas.pottner@gmail.com>
2025-05-26 19:44:02 +0200

    Remove unused error variable

--- frontend/src/services/api-activities.ts
+++ frontend/src/services/api-activities.ts
@@ -38,7 +38,7 @@ export async function fetchActivities({
   try {
     const parsedData = activitiesAPIResponseSchema.parse(rawData)
     return parsedData
-  } catch (error) {
+  } catch {
     return { products: [], totalCount: 0 }
   }
 }
